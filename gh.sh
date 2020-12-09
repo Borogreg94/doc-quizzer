9dcd6259f8eece69134dd1ccc4cfa2704160007d
gborodulin <borogreg@gmail.com>
2020-12-09 16:49:49 -0500

    remove shell script

--- gh.sh
+++
@@ -1,6 +0,0 @@
-git filter-branch -f --env-filter "
-    GIT_AUTHOR_NAME='gborodulin'
-    GIT_AUTHOR_EMAIL='borogreg@gmail.com'
-    GIT_COMMITTER_NAME='gborodulin'
-    GIT_COMMITTER_EMAIL='borogreg@gmail.com'
-  " HEAD

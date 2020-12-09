22178d867f9d292a7d1f65f3f11e44b85d074ea9
gborodulin <borogreg@gmail.com>
2020-12-09 17:53:03 -0500

    Updates

---
+++ precache-manifest.175cd6d89b540e6f6e79691482aa3ec7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26dfb86420b9e9fb0c7d7c9bae895c90",
+    "url": "/music-quizzer/index.html"
+  },
+  {
+    "revision": "d1e60147b2b1ee4a03c3",
+    "url": "/music-quizzer/static/css/main.1d69a3e0.chunk.css"
+  },
+  {
+    "revision": "c5e867e0286f8d15f577",
+    "url": "/music-quizzer/static/js/2.3b500c8e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/music-quizzer/static/js/2.3b500c8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e60147b2b1ee4a03c3",
+    "url": "/music-quizzer/static/js/main.615285df.chunk.js"
+  },
+  {
+    "revision": "92d44494ceb45db172ed",
+    "url": "/music-quizzer/static/js/runtime-main.6a06bd0b.js"
+  },
+  {
+    "revision": "f142568f1238b479112e6cedd35b776a",
+    "url": "/music-quizzer/static/media/HighlandGothicFLF.f142568f.ttf"
+  }
+]);
